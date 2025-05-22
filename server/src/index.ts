8a95490ea773c08edad1af459d8a572d49e26455
Mosmit <sirmosmit10@gmail.com>
2025-05-22 19:05:49 +0100

    getting-started

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import mongoose from "mongoose";
+
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("hello world mosmit");
+const db = mongoose.connect("mongodb+srv://sirmosmit10:Mosmit10$@cluster0.wifaipd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("hello world mos");
 });
 
 app.listen(5000);
